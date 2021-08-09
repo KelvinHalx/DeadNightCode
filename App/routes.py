626d9e03f7da1a665fc587b8d31ef9c2a94f98e2
KelvinHalx <kelvinwainaina03@gmail.com>
2021-08-09 13:33:48 +0300

    Create routes.py

---
+++ App/routes.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template, url_for
+from App import app
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/base/')
+def base():
+    return render_template('base.html')
+
